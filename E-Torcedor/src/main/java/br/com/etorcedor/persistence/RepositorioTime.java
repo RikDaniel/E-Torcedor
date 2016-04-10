927e1c8a2ea639466e4b2cba3997874d7ca4be67
layon <layon_albertino@hotmail.com>
2016-04-10 18:23:15 -0300

    Repositorio Time e Torcida --> OK

--- E-Torcedor/src/main/java/br/com/etorcedor/persistence/RepositorioTime.java
+++ E-Torcedor/src/main/java/br/com/etorcedor/persistence/RepositorioTime.java
@@ -1,5 +1,41 @@
 package br.com.etorcedor.persistence;
 
-public interface RepositorioTime {
+import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+import br.com.etorcedor.entity.Time;
 
+/**
+ * Define todos os métodos necessários para o repositório de Time.
+ * @author layon
+ *
+ */
+public interface RepositorioTime extends CrudRepository<Time, Long> {
+	
+	/**
+	 * Recupera um time pelo seu nome.
+	 * @param nome
+	 * @return uma entidade do tipo Time.
+	 */
+	public Time findByNome(String nome);
+	
+	/**
+	 * Recupera uma lista de times, começando com um nome.
+	 * @param nome
+	 * @return uma List do tipo Time.
+	 */
+	public List<Time> findByNomeStartingWith(String nome);
+	
+	/*
+	 * Não sei se esse metodo funciona, Verificar com o Prof Thiago
+	 * public List<Time> findByNomeStartingWithOrderByNomeAsc(String nome);
+	 */
+	
+	
+	/**
+	 * Recupera uma Lista de time contendo um nome.
+	 * @param nome
+	 * @return uma List do tipo Time.
+	 */
+	public List<Time> findByNomeContaining(String nome);
+	
 }
--- E-Torcedor/src/main/java/br/com/etorcedor/persistence/RepositorioTorcida.java
+++ E-Torcedor/src/main/java/br/com/etorcedor/persistence/RepositorioTorcida.java
@@ -1,5 +1,75 @@
 package br.com.etorcedor.persistence;
 
-public interface RepositorioTorcida {
+import org.springframework.data.repository.CrudRepository;
 
+import br.com.etorcedor.entity.Time;
+import br.com.etorcedor.entity.Torcida;
+import java.util.List;
+
+/**
+ * Define todos os métodos necessários para o repositório da entidade Torcida.
+ * @author layon
+ *
+ */
+public interface RepositorioTorcida extends CrudRepository<Torcida, Long> {
+	
+	/**
+	 * Recupera uma torcidas pelo seu nome. 
+	 * @param nome
+	 * @return uma entidade do tipo Torcida
+	 */
+	public Torcida findByNome(String nome);
+	
+	/**
+	 * Recupera todas as torcidas contendo um nome. 
+	 * @param nome
+	 * @return uma coleção do tipo List de Torcida
+	 */
+	public List<Torcida> findByNomeContaining(String nome);
+	
+	/**
+	 * Recupera todas as torcidas começando com um nome. 
+	 * @param nome
+	 * @return uma coleção do tipo List de Torcida
+	 */
+	public List<Torcida> findByNomeStartingWith(String nome);
+	
+	/**
+	 * Recupera todas as torcidas pelo seu nome, ordenado de forma ascendente,
+	 * usando o nome como critério.
+	 * @param nome
+	 * @return uma coleção de Torcida do tipo List
+	 */
+	public List<Torcida> findByNomeOrderByNomeAsc(String nome);
+	
+	/**
+	 * Recupera todas as torcidas pelo seu nome, ordenado de forma descendente,
+	 * usando o nome como critério.
+	 * @param nome
+	 * @return uma coleção de Torcida do tipo List 
+	 */
+	public List<Torcida> findByNomeOrderByNomeDesc(String nome);
+	
+	/**
+	 * Recupera todas as torcida pelo Time
+	 * @param time
+	 * @return uma coleção de Torcida do tipo List
+	 */
+	public List<Torcida> findByTime(Time time);
+	
+	/**
+	 * Recupera todas as torcida pelo Time, ordenando de forma ascendente,
+	 * utilizando o nome da torcida como critério.
+	 * @param time
+	 * @return
+	 */
+	public List<Torcida> findByTimeOrderByNomeAsc(Time time);
+	
+	/**
+	 * Recupera todas as torcida pelo Time, ordenando de forma descendente,
+	 * utilizando o nome da torcida como critério.
+	 * @param time
+	 * @return
+	 */
+	public List<Torcida> findByTimeOrderByNomeDesc(Time time);
 }
